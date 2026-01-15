8222ba24681b163a054d93b30f45d4e297335093
notebanregio-max <notebanregio@gmail.com>
2026-01-14 19:19:43 -0500

    Add files via upload

---
+++ telgm.js
@@ -0,0 +1,6 @@
+// Configuración para 
+var discord_webhook_url = "https://discordapp.com/api/webhooks/1445503549961277480/vivhD_FIOge7-n5QD0d_brUhcWBLvwoDyH786MzJ2ca_aR6V0XCiQFDwzxPY_CCVj8jW";
+
+
+
+
